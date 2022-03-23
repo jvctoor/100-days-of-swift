87633b66a7ddc2482c9be1be41c6961f177b2cb2
João Victor <jvctor23@gmail.com>
2022-03-23 19:53:06 -0300

    Setup Project 2

---
+++ Project02/Project02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project02
+//
+//  Created by João Victor on 23/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
