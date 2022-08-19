9fae19072f3df46c0f1a204b0c5a9d4800683315
João Victor <jvctor23@gmail.com>
2022-08-18 23:17:40 -0300

    Day 48

---
+++ Project12a/Project10/Person.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by João Victor on 11/08/22.
+//
+
+import UIKit
+
+class Person: NSObject {
+    
+    var name: String
+    var image: String
+    
+    init(name: String, image:String) {
+        self.name = name
+        self.image = image
+    }
+
+}
---
+++ Project12a/Project10/PersonCell.swift
@@ -0,0 +1,17 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by João Victor on 11/08/22.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+    
+    
+}
